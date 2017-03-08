2ae3f387e93928402d1c73cb83a484737b4b5a0c
CristianDmt <chucky.modo@yahoo.com>
2017-03-08 20:39:40 +0200

    Preparing Initial Boilerplate

---
+++ models/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var fs = require("fs");
+var path = require("path");
+var env = process.env.NODE_ENV || "development";
+var config = require(__dirname + '/../config/config.json')[env];
+
+var db = require('mongoose');
+db.connect(config.protocol + '://' + config.host + '/' + config.database);
+
+module.exports = db;
