3a6cc1106e604bade1584724b5dcdb42a727f8ed
CristianDmt <chucky.modo@yahoo.com>
2017-04-08 00:17:59 +0300

    Tweaked the Pager to Allow Status Responses and to Properly Stop Execution of the Script if Validation Fails

--- helpers/PageResponses.js
+++ helpers/PageResponses.js
@@ -1,8 +1,30 @@
 "use strict";
 
-exports.accessDenied = function() {
+exports.accessDenied = function(res) {
     return res.json({
         status: 'error',
         response: 'access_denied'
+    });
+}
+
+exports.jsonOk = function(res, str) {
+    return res.json({
+        status: 'ok',
+        response: str
+    });
+}
+
+exports.jsonData = function(res, str, data) {
+    return res.json({
+        status: 'ok',
+        response: str,
+        data: data
+    });
+}
+
+exports.jsonError = function(res, error) {
+    return res.json({
+        status: 'error',
+        response: error
     });
 }
