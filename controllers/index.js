611568a36d8402168c3db40337cf28c4af11420c
CristianDmt <chucky.modo@yahoo.com>
2017-04-20 04:21:19 +0300

    Replaced Random Message with API Response Format

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,6 @@
 exports.index = function(req, res) {
-    res.json({ goaway: "How'd you get to this API's endpoint? o_O"});
+    res.json({
+        status: "error",
+        response: "unknown_request"
+    });
 }
