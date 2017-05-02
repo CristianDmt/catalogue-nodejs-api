553645d3021c8272329301b64301f6b99cae61cc
CristianDmt <chucky.modo@yahoo.com>
2017-05-02 04:11:08 +0300

    Not Found Auth has to Return False

--- models/Auth.js
+++ models/Auth.js
@@ -95,7 +95,7 @@ exports.matchAuth = function(authUsername, authPassword, callback) {
             });
         }
         else {
-            return callback(null, null, true);
+            return callback(null, null, false);
         }
     }
 
