ced6692f33ef3157b400c76c859225bffb0efc77
CristianDmt <chucky.modo@yahoo.com>
2017-05-04 03:00:39 +0300

    Tweaked REST Middleware Headers to Allow Foreign Requests

--- middleware/Origin.js
+++ middleware/Origin.js
@@ -3,6 +3,7 @@
 exports.enableREST = function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, X-Requested-With');
     res.setHeader('Access-Control-Allow-Credentials', true);
 
     return next();
